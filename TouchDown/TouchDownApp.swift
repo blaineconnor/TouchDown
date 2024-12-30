1008c59fbd58edfb84501dbee686ceb538d7b96f
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2024-12-30 14:57:13 +0300

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Fatih Emre on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

cb64a27d77cbb72fa04fac2041dc338235fec924
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2024-12-31 10:47:08 +0300

    fix: ContentView has recreated cause of the bug.

---
+++ TouchDown/App/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  TouchDown
+//
+//  Created by Fatih Emre on 30.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top) // for setting the screen to current device
+                    .background(.white)
+                    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .padding(.vertical, 20)
+                            .frame(height: 300)
+                        
+                        CategoryGridView()
+                        FooterView()
+                            .padding(.horizontal)
+                    }//: VSTACK
+                })//: SCROLL
+                
+                
+            }//: VSTACK
+            .background(colorBackground.ignoresSafeArea(.all,edges: .all))
+        }//: ZSTACK
+        .ignoresSafeArea(.all,edges: .top)
+    }
+}
+
+// MARK: - PREVIEW
+#Preview {
+    ContentView()
+}

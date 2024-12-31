f8dd6069886510d3bafe00de9cdb98f505037d68
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2024-12-31 10:37:17 +0300

    feat: category model created and data has been added.

---
+++ TouchDown/Model/CategoryModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryModel.swift
+//  TouchDown
+//
+//  Created by Fatih Emre on 30.12.2024.
+//
+
+struct Category: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
